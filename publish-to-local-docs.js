46c9cca6f02f76b33f7722f265a16ae258d38de7
Andrew Harmel-Law <andrew@harmel-law.com>
2017-09-04 20:41:54 +0100

    Remove the publisher js (It's in binder-cli now)

--- publish-to-local-docs.js
+++
@@ -1,6 +0,0 @@
-var docPublisher = require('doc-publisher');
-var pkginfo = require('pkginfo')(module, 'dependencies');
-
-// Pull the skill dependency keys from the package.json file 
-const dependencyKeys = Object.keys(module.exports.dependencies);
-docPublisher(dependencyKeys);
